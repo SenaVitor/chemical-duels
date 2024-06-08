55c29543319794001bdae6afd90d397312ebc65c
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2024-06-07 22:48:45 -0300

    change sprites to static

--- client/src/helpers/cards/Element.js
+++ client/src/helpers/cards/Element.js
@@ -1,6 +1,10 @@
 import Card from "./Card";
 
 export default class Element extends Card {
+    static sprites = [
+        "br", "c", "ca", "cl", "f", "h", "k", "mg", "mn", "n", "na", "o", "s", "transmutacao", "lavoisier",
+        "br", "c", "ca", "cl", "f", "h", "k", "mg", "mn", "n", "na", "o", "s", "transmutacao", "lavoisier"
+    ];
     constructor(scene, sprite) {
         super(scene);
         this.name = "element";
@@ -8,10 +12,10 @@ export default class Element extends Card {
             this.playerCardSprite = sprite;
             this.opponentCardSprite = sprite;
         }else {
-            const sprites = ["br", "c", "ca", "cl", "f", "h", "k", "mg", "mn", "n", "na", "o", "s", "transmutacao", "lavoisier"];
-            const index = Math.floor(Math.random() * sprites.length);
-            this.playerCardSprite = sprites[index];
-            this.opponentCardSprite = sprites[index];
+            const index = Math.floor(Math.random() * Element.sprites.length);
+            this.playerCardSprite = Element.sprites[index];
+            this.opponentCardSprite = Element.sprites[index];
+            Element.sprites.splice(index, 1);  
         }
     }
 }
